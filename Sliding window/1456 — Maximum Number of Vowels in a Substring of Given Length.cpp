480d03906ee170049214d88752189bd9a07bded7
Krishika25 <krishikad25@gmail.com>
2026-02-18 21:28:49 +0530

    Fixed Sliding window problem

---
+++ Sliding window/1456 — Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -0,0 +1,35 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isvowel(char c) {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+
+    int maxVowels(string s, int k) {
+        int count = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(isvowel(s[i])) {
+                count++;
+            }
+        }
+
+        int maxCount = count;
+
+        for(int i = k; i < s.size(); i++) {
+            if(isvowel(s[i])) {
+                count++;
+            }
+            if(isvowel(s[i - k])) {
+                count--;
+            }
+            if(count > maxCount) {
+                maxCount = count;
+            }
+        }
+
+        return maxCount;
+    }
+};
