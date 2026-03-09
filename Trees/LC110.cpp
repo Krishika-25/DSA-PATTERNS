4f79ec0bc4cb3d6188e686bddecc3ed01613e1d5
Krishika-dev <krishikad25@gmail.com>
2026-03-09 22:48:08 +0530

    Solved LC 110

---
+++ Trees/LC110.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    int height(TreeNode *root)
+    {
+        int left = 0;
+        int right = 0;
+        if (root == NULL)
+        {
+            return 0;
+        }
+        left = height(root->left);
+        right = height(root->right);
+        if (left == -1 || right == -1)
+            return -1;
+
+        if (abs(left - right) > 1)
+            return -1;
+        return 1 + max(left, right);
+    }
+    bool isBalanced(TreeNode *root)
+    {
+        return height(root) != -1;
+    }
+};
