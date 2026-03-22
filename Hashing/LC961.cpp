84cca58f322f1472ef48537f3ebf95fbe8e4ce2b
Krishika-dev <krishikad25@gmail.com>
2026-03-22 20:53:53 +0530

    Solved LC 961

---
+++ Hashing/LC961.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& a) {
+        int n=a.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+            mp[a[i]]++;
+        for(auto i:mp)
+        {
+            if(i.second>=2)
+                return i.first;
+        }
+        return 0;
+    }
+};
