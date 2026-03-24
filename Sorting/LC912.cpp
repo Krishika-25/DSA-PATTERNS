c610ac60befeb51221523c5539c35dcd9ccac174
Krishika-dev <krishikad25@gmail.com>
2026-03-24 21:46:51 +0530

    Solved LC 912

---
+++ Sorting/LC912.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int partition(vector<int>& nums,int low,int high){
+        int pivot=nums[low];
+        int i=low;
+        int j=high;
+        while(i<j){
+            while(nums[i]<=pivot && i<=high-1){
+                i++;
+            }
+            while(nums[j]>pivot && j>=low+1){
+                j--;
+            }
+            if(i<j){
+                swap(nums[i],nums[j]);
+            }
+        }
+        swap(nums[low],nums[j]);
+        return j;
+    }
+    void quicksort(vector<int>& nums,int low,int high){
+        if(low<high){
+        int partitionIndex= partition(nums,low,high);
+        quicksort(nums,low,partitionIndex-1);
+        quicksort(nums,partitionIndex+1,high);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        quicksort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
