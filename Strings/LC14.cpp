548a09f1a17acbb882ba2f1e093dc636a2aafa5f
Krishika-dev <krishikad25@gmail.com>
2026-04-06 22:32:38 +0530

    Solved LC 14

---
+++ Strings/LC14.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) {
+            return "";
+        }
+        string ans = "";
+        for (int i = 0; i < strs[0].size(); i++) {
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != strs[0][i]) {
+                    return ans;
+                }
+            }
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
